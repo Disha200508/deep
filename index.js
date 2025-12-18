7002007bcfdf6574eec2743c0d6f27359bfcc1d5
Disha200508 <dishadeshwal0508@gmail.com>
2025-12-19 00:26:12 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./App.css";
-import App from "./App";
+require("dotenv").config();
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+const authRoutes = require("./routes/auth");
+const chatRoutes = require("./routes/chat");
+const chatStorageRoutes = require("./routes/chats");
+const bookmarkRoutes = require("./routes/bookmark");
+const app = express();
+/* middleware */
+app.use(cors());
+app.use(express.json());
+
+/* routes */
+app.use("/auth", authRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/chats", chatStorageRoutes);
+app.use("/api/bookmarks",bookmarkRoutes);
+app.get("/health", (req, res) => {
+  res.json({ status: "Backend is running fine ✅" });
+});
+
+app.get("/", (req, res) => {
+  res.send("✅ Backend is running");
+});
+
+/* MongoDB */
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error(err.message));
+
+/* server */
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
